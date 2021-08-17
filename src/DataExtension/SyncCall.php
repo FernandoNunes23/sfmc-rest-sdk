42eeffb60639288f16e69ec64bf88db20335f9a3
Fernando Nunes Nunes <CIANDT\fernandonunes@lnb028004bhz.ciandt.global>
2021-08-17 14:27:00 -0300

    Changed filter method to be possible to receive null

--- src/DataExtension/SyncCall.php
+++ src/DataExtension/SyncCall.php
@@ -53,20 +53,45 @@ class SyncCall
         return $this->client->postJson($uri, [$data]);
     }
 
+    /**
+     * @param string $dataExtensionKey
+     * @param int|null $page
+     * @param int|null $pageSize
+     * @param string|null $orderBy
+     * @param string|null $filters
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Phpfastcache\Exceptions\PhpfastcacheSimpleCacheException
+     * @throws \Psr\Cache\InvalidArgumentException
+     */
     public function filterRows(
         string $dataExtensionKey,
-        int $page,
-        int $pageSize,
-        string $orderBy,
-        string $filters
+        ?int $page = null,
+        ?int $pageSize = null,
+        ?string $orderBy = null,
+        ?string $filters = null
     )
     {
         $uri = "data/v1/customobjectdata/key/{$dataExtensionKey}/rowset";
 
-        $query['$page'] = $page;
-        $query['$pageSize'] = $pageSize;
-        $query['$orderBy'] = $orderBy;
-        $query['$filter'] = $filters;
+        $query = [];
+
+        if ($page) {
+            $query['$page'] = $page;
+        }
+
+        if ($pageSize) {
+            $query['$pageSize'] = $pageSize;
+        }
+
+        if ($orderBy) {
+            $query['$orderBy'] = $orderBy;
+        }
+
+        if ($filters) {
+            $query['$filter'] = $filters;
+        }
 
         return $this->client->get($uri, $query);
     }
